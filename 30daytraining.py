b05a0bf3620de1df13deeccfa80ee52ce2ffd3cd
George <39542358+George0417@users.noreply.github.com>
2023-08-02 10:12:40 +0900

    Update 30daytraining.py

--- 30daytraining.py
+++ 30daytraining.py
@@ -1,4 +1,11 @@
 import streamlit as st
+from datetime import time, datetime
 
 st.header('Hi George')
-st.write(':sunglasses:')
+st.write('It is a good day today :sunglasses:')
+
+st.header('st.slider')
+st.subheader('slider')
+
+age=st.slider('How old are you', 0, 100, 5)
+st.write('I'm', age, 'years old')
