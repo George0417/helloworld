1464922f01f398cd82b30c5ec504ad922fa684d3
George <39542358+George0417@users.noreply.github.com>
2023-08-02 10:19:11 +0900

    Update 30daytraining.py

--- 30daytraining.py
+++ 30daytraining.py
@@ -9,3 +9,8 @@ st.subheader('slider')
 
 age=st.slider('How old are you', 0, 100, 5)
 st.write("I'm", age, 'years old')
+
+st.subheader('Range slider')
+
+values=st.slider('select a range of value', 0.0,100.0,(25.0,75.0))
+st.write('values:',values)
