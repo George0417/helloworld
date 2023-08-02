2937d7302ab7b4a8ebf1193a9e17f12357f8c0e9
George <39542358+George0417@users.noreply.github.com>
2023-08-02 10:22:52 +0900

    Update 30daytraining.py

--- 30daytraining.py
+++ 30daytraining.py
@@ -1,3 +1,4 @@
+#DAY 8
 import streamlit as st
 from datetime import time, datetime
 
@@ -22,3 +23,11 @@ appointment = st.slider(
      "Schedule your appointment:",
      value=(time(11, 30), time(12, 45)))
 st.write("You're scheduled for:", appointment)
+
+st.subheader('Datetime slider')
+
+start_time = st.slider(
+     "When do you start?",
+     value=datetime(2020, 1, 1, 9, 30),
+     format="MM/DD/YY - hh:mm")
+st.write("Start time:", start_time)
