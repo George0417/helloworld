baa0d467828a7cf8aed56e009a7baae87012713a
George <39542358+George0417@users.noreply.github.com>
2023-08-03 10:58:06 +0900

    Update Day8_9.py

--- Day8_9.py
+++ Day8_9.py
@@ -1,1 +1,16 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+st.header('HI george')
+st.write('Have a good day! :sunglasses:')
+@st.cache
+
+st.header('DAY 9 st.line_chart')
+
+chart_data=pd.DataFrame(
+  np.random.randn(20,3),
+  columns=['a','b','c']
+)
+
+st.lin_chart(chart_data)
