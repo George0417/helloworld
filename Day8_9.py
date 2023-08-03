3311a678cf19dfac06f6cd2694e175442de46f3d
George <39542358+George0417@users.noreply.github.com>
2023-08-03 11:06:44 +0900

    Update Day8_9.py

--- Day8_9.py
+++ Day8_9.py
@@ -5,7 +5,7 @@ import numpy as np
 st.header('HI george')
 st.write('Have a good day! :sunglasses:')
 
-
+@st.cache
 st.subheader('DAY 9 st.line_chart')
 
 chart_data=pd.DataFrame(
@@ -14,3 +14,12 @@ chart_data=pd.DataFrame(
 )
 
 st.line_chart(chart_data)
+
+
+st.subheader('DAY 10 st.selectbox')
+option=st.selectbox(
+  'how many kids do you have?'
+  ('1','2','3','4')
+)
+
+st.write('your kids number is ', option)
