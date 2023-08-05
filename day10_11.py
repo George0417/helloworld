65873615ee6c688392cfc8fb7d04c66ad38affb6
George <39542358+George0417@users.noreply.github.com>
2023-08-05 12:46:34 +0900

    Update day10_11.py

--- day10_11.py
+++ day10_11.py
@@ -1,1 +1,12 @@
 
+st.subheader('DAY 10 st.selectbox')
+option=st.selectbox(
+  'how many kids do you have?',
+  ('1','2','3','4')
+)
+
+
+st.subheader('Day11 st.multiselect')
+options=st.multiselect('what is your faviote color',['blue','green'],['yellow','red'])
+st.write('You selected:', options)
+st.write('your kids number is ', option)
